2cc74502a38574dbbb03fc6e01bf9053c2db9536
yuvrajrsingh <139677438+yuvrajrsingh@users.noreply.github.com>
2025-01-02 17:18:49 +0530

    Update task.py

--- Day 2/task.py
+++ Day 2/task.py
@@ -1,5 +1,5 @@
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
+people = int(input("How many people to split the bill?" ))
 print(f"Each person should pay: ${round(bill*(1+(tip/100))/people,2)}")
